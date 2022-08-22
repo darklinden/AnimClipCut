31c1d8ab521453012b47db9838f09ee6043db860
darklinden <darklinden@gmail.com>
2022-08-22 18:44:50 +0800

    fix bug: export sprite sheet crash when crop area not match

--- Assets/Plugins/Editor/ExportSpriteSheets.cs
+++ Assets/Plugins/Editor/ExportSpriteSheets.cs
@@ -17,14 +17,56 @@ public class ExportSubSprites {
 		}
 	}
 
+	private static Texture2D CropTexture(Texture2D pSource, int left, int top, int width, int height) {
+		if (left < 0) {
+			width += left;
+			left = 0;
+		}
+		if (top < 0) {
+			height += top;
+			top = 0;
+		}
+		if (left + width > pSource.width) {
+			width = pSource.width - left;
+		}
+		if (top + height > pSource.height) {
+			height = pSource.height - top;
+		}
+
+		if (width <= 0 || height <= 0) {
+			return null;
+		}
+
+		Color[] aSourceColor = pSource.GetPixels(0);
+
+		//*** Make New
+		Texture2D oNewTex = new Texture2D(width, height, TextureFormat.RGBA32, false);
+
+		//*** Make destination array
+		int xLength = width * height;
+		Color[] aColor = new Color[xLength];
+
+		int i = 0;
+		for (int y = 0; y < height; y++) {
+			int sourceIndex = (y + top) * pSource.width + left;
+			for (int x = 0; x < width; x++) {
+				aColor[i++] = aSourceColor[sourceIndex++];
+			}
+		}
+
+		//*** Set Pixels
+		oNewTex.SetPixels(aColor);
+		oNewTex.Apply();
+
+		//*** Return
+		return oNewTex;
+	}
+
 	// Since a sprite may exist anywhere on a tex2d, this will crop out the sprite's claimed region and return a new, cropped, tex2d.
 	private static Texture2D ExtractAndName(Sprite sprite) {
-		var output = new Texture2D((int)sprite.rect.width, (int)sprite.rect.height);
 		var r = sprite.textureRect;
-		var pixels = sprite.texture.GetPixels((int)r.x, (int)r.y, (int)r.width, (int)r.height);
-		output.SetPixels(pixels);
-		output.Apply();
-		output.name = sprite.texture.name + " " + sprite.name;
+		var output = CropTexture(sprite.texture, (int)r.x, (int)r.y, (int)r.width, (int)r.height);
+		output.name = sprite.name;
 		return output;
 	}
 
